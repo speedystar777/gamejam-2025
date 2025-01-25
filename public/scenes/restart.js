8ce38209f3816c613c0f96c013685c11e53b0b5d
Preeti <pnu3@cornell.edu>
2025-01-25 18:14:54 -0500

    High score working

--- public/scenes/restart.js
+++ public/scenes/restart.js
@@ -4,20 +4,54 @@ class Restart extends Phaser.Scene {
   }
   init(data) {
     this.finalScore = data.score;
+    this.currentHighScore = data.currentHighScore;
   }
   preload() {
-    this.load.image('sky', 'assets/orig_big.png');
+    this.load.image("sky", "assets/orig_big.png");
   }
   create() {
-    this.add.image(400, 300, 'sky');
-    this.add.text(window.innerWidth / 2 - 250, window.innerHeight / 2 - 100, 'Game Over!', { fontSize: 50, fontStyle: "bold" });
-    this.add.text(window.innerWidth / 2 - 250, window.innerHeight / 2 - 50, 'Your score was: ' + this.finalScore, { fontSize: 50, fontStyle: "bold" });
-    this.add.text(window.innerWidth / 2 - 250, window.innerHeight / 2 + 50, 'Click to restart', { fontSize: 30 });
+    this.add.image(400, 300, "sky");
+    this.add.text(
+      window.innerWidth / 2 - 250,
+      window.innerHeight / 2 - 200,
+      "Game Over!",
+      { fontSize: 100, fontStyle: "bold" }
+    );
+    this.add.text(
+      window.innerWidth / 2 - 250,
+      window.innerHeight / 2 - 60,
+      "High Score: " + `${
+          this.finalScore > this.currentHighScore
+            ? this.finalScore
+            : this.currentHighScore
+        }`,
+      { fontSize: 50, fontStyle: "bold" }
+    );
+    this.add.text(
+      window.innerWidth / 2 - 250,
+      window.innerHeight / 2 - 15,
+      "Your score was: " + this.finalScore,
+      { fontSize: 50, fontStyle: "bold" }
+    );
+    this.add.text(
+      window.innerWidth / 2 - 250,
+      window.innerHeight / 2 + 60,
+      "Click to restart",
+      { fontSize: 30 }
+    );
   }
 
   update() {
-    this.input.on('pointerdown', function () {
-      this.scene.start('game');
-    }, this)
+    let newHighScore = this.currentHighScore;
+    if (this.currentHighScore < this.finalScore) {
+      newHighScore = this.finalScore;
+    }
+    this.input.on(
+      "pointerdown",
+      function () {
+        this.scene.start("game", { highScore: newHighScore });
+      },
+      this
+    );
   }
 }
