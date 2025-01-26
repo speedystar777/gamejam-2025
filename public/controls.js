e485c70305be74b5a2f42a7990ba97801be12175
Charles Oliner <github.3yhhb@charlesoliner.com>
2025-01-26 13:09:49 -0500

    Added instructions

--- public/controls.js
+++ public/controls.js
@@ -37,30 +37,3 @@ function setController(newController) {
 function cameraAvailable() {
     return !cameraPending && video.srcObject?.active && video.videoWidth && video.videoHeight;
 }
-
-function controlsSelectionCreate(scene, y = null){
-    scene.mouseControls = scene.add.text(window.innerWidth / 2 - 150, y ?? (window.innerHeight / 2), 'mouse controls', { fontSize: 20 }).setInteractive().setOrigin(0.5);
-    scene.cameraControls = scene.add.text(window.innerWidth / 2 + 150, y ?? (window.innerHeight / 2), 'camera controls\n(use right hand)', { fontSize: 20 }).setInteractive().setOrigin(0.5);
-
-    if (controller === "mouse") {
-        scene.mouseControls.setTint(0xfc49dc);
-        scene.cameraControls.clearTint();
-    } else {
-        scene.cameraControls.setTint(0xfc49dc);
-        scene.mouseControls.clearTint();
-    }
-}
-
-function controlselectionUpdate(scene) {
-    scene.mouseControls.on('pointerdown', function () {
-        setController("mouse");
-        scene.mouseControls.setTint(0xfc49dc);
-        scene.cameraControls.clearTint();
-    }, this);
-
-    scene.cameraControls.on('pointerdown', function () {
-        setController("camera");
-        scene.cameraControls.setTint(0xfc49dc);
-        scene.mouseControls.clearTint();
-    }, this);
-}
